680da314794b8c30490c92fce30d8a8aa6d7a0a6
Eric Madureira <ericmadu416@gmail.com>
2025-09-02 08:58:40 -0300

    deck nav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import defaultDeck from "../src/decks/default.json"
 import Card from './components/Card'
+import DeckNav from './components/DeckNav'
 
 function App() {
   const [selectedDeck, setSelectedDeck] = useState(defaultDeck)
   return (
     <div className='flex flex-col text-center'>
 			<p className='mt-8'>Deck title: { selectedDeck.title }</p>
+			<DeckNav />
       {selectedDeck.cards.map(card =>
 				<Card key={card.question} question={card.question} answer={card.answer} />
 			)}
---
+++ src/components/DeckNav.tsx
@@ -0,0 +1,10 @@
+const DeckNav = () => {
+	return (
+		<div className="flex border max-w-60 justify-around mt-4">
+			<button className="previousCard">⬅</button>
+			<button className="nextCard">➡</button>
+		</div>
+	)
+}
+
+export default DeckNav
